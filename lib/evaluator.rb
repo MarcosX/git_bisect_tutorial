a6a522468b46886c690f672299bb096d19a835ae
Marcos Brizeno <marcos.uece.comp@gmail.com>
2012-11-05 21:13:37 -0300

    Player 1 wins

--- lib/evaluator.rb
+++ lib/evaluator.rb
@@ -1,2 +1,11 @@
 class Evaluator
+  def evaluate player1, player2
+    if player1 == 'rock' && player2 == 'scissors'
+      return 1
+    elsif player1 == 'scissors' && player2 == 'paper'
+      return 1
+    elsif player1 == 'paper' && player2 == 'rock'
+      return 1
+    end
+  end
 end
